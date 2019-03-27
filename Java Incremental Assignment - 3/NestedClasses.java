d919fbe987bd93c9c2932ef5bed9329519a00dff
Rohith Reddy <38779465+rohithreddy197@users.noreply.github.com>
2019-03-27 15:31:21 +0530

    Create NestedClasses.java

---
+++ Java Incremental Assignment - 3/NestedClasses.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+class Outer1{
+    Outer1(){
+        System.out.println("Outer class 1");
+    }
+    class Inner1{ //nested class1
+        int n;
+        Inner1(int n){
+            this.n=n;
+            System.out.println("Inner class in Outer1 "+n);
+        }
+    }
+}
+class Outer2{
+    Outer2(){
+        System.out.println("Outer class 2");
+    }
+    class Inner2 extends Outer1.Inner1{ //nested class extending other nested class
+        Inner2(Outer1 a){
+            a.super(22); 	//calling nested class constructor
+            System.out.println("Inner class2 extending Inner1");
+        }
+    }
+}
+class NestedClasses{
+    public static void main(String a[]){
+        Outer1 o1=new Outer1();                        //creating object for outer class
+        Outer2.Inner2 o2=new Outer2().new Inner2(o1); //creating second nested class's object
+    }
+}
