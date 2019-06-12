ad36f76e2185e24c61702e8035c47954878947f5
Rohith Reddy <38779465+rohithreddy197@users.noreply.github.com>
2019-06-12 18:04:58 +0530

    Update ReadFile.java

--- ReadFile.java
+++ ReadFile.java
@@ -1,41 +1,40 @@
-import java.util.HashMap;
-import java.io.File;
 import java.io.*;
-import java.util.*;
-import java.lang.String;
-   public class ReadFile{
-           public static void main(String args[]){
-              //System.out.println("Frst");
-            try{
-            //FileReader f=new FileReader("inputfile.txt");
-		          File f=new File("inputtext.txt");
-	    	      BufferedReader br=new BufferedReader(new FileReader(f));
-          	  //System.out.println("Scnd");
-          	  HashMap< Character , Integer > hm=new HashMap< Character , Integer>();
-	            //int i;
-	             /*
-                    while((i==f.read())!=false){
-	                  System.out.print((char)i);
-	                  System.out.println("frth");
-	                  }
-		          */
-		          String s;
-		          while((s=br.readLine())!=null){
-		          char[] schar=s.toCharArray();
-		          //for(int i=0;i<s.length();i++){
-		          for(char c: schar){
-		            if(hm.containsKey(c)){
-			              hm.put(c,hm.get(c)+1);
-		            }
-		            else hm.put(c,1);
-	  	          //System.out.println("THRd");
-		            //System.out.println(s);
-		         }		
-	        }	
-		         for(Map.Entry entry:hm.entrySet()){
-			          System.out.println(entry.getKey()+" "+entry.getValue());
-		      }
-	  }
-	  catch(Exception E){}
-           }
-  }
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReadFile {
+    public static void main(String args[]){
+        try{
+	    //Reading input file
+            File f=new File("/home/lenovo/Desktop/inputtext.txt");
+            BufferedReader br=new BufferedReader(new FileReader(f));
+            HashMap< Character , Integer > hm=new HashMap< Character , Integer>();
+            String s;
+            while((s=br.readLine())!=null){
+                char[] schar=s.toCharArray();
+		//adding alphabet occurances to hashmap
+                for(char c: schar){
+                    if(hm.containsKey(c)){
+                        hm.put(c,hm.get(c)+1);
+                    }
+                    else hm.put(c,1);
+                }
+            }
+	    //printing character occurance values
+            for(Map.Entry entry:hm.entrySet()){
+                System.out.println(entry.getKey()+" "+entry.getValue());
+            }
+        }
+	//Exception handling
+        catch(FileNotFoundException E) {
+            System.out.println("File Not Found");
+        }
+        catch (IndexOutOfBoundsException E){
+            System.out.println("Index out of bounds");
+        }
+
+        catch(IOException E){
+            System.out.println("IO exception");
+        }
+    }
+}
